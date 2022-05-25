886ae5eea8c2787f462ac7b28de61db3d7bc006b
Mesi <mse212002@gmail.com>
2022-05-25 19:34:00 +0300

    :art: style all components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import '../Styles/Footer.css';
 
 const Footer = () => (
   <footer className="fixed-bottom bg-dark" id="footer">
-    <div>
+    <div className="flex-row">
       <div>
         <p>&copy; Mesi 2022</p>
-        <p><FontAwesomeIcon icon={faFacebookF} /></p>
+      </div>
+      <div className="social flex-row">
+        <p><a href="https://www.facebook.com/emese.molnar.397/" aria-label="fb"><FontAwesomeIcon icon={faFacebookF} /></a></p>
+        <p><a href="https://github.com/Mesi21" aria-label="gh"><FontAwesomeIcon icon={faGithub} /></a></p>
+        <p><a href="https://www.linkedin.com/in/emesemesimolnar/" aria-label="LNk"><FontAwesomeIcon icon={faLinkedin} /></a></p>
       </div>
     </div>
   </footer>
--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRotateBackward } from '@fortawesome/free-solid-svg-icons';
+import { FaRegArrowAltCircleLeft } from 'react-icons/fa';
 import { getData } from '../Redux/countries/countries';
 import '../Styles/Details.css';
 
@@ -19,45 +18,55 @@ const Details = () => {
   const handleClick = () => navigate('/home');
   return (
     <div className="detailsPage flex-column">
-      <div><FontAwesomeIcon icon={faArrowRotateBackward} onClick={handleClick} /></div>
-      <div>
+      <div className="back"><FaRegArrowAltCircleLeft onClick={handleClick} /></div>
+      <div className="allStats">
+        <h1>{selectedCountry[0].name}</h1>
+        <img src={selectedCountry[0].flag} alt="flag" />
         <ul>
-          <li className="flex-row">
-            <h1>{selectedCountry[0].name}</h1>
+          <li className="stats">
+            <em>
+              Capital city:
+              {' '}
+              {selectedCountry[0].capital}
+            </em>
           </li>
-          <li><h3>{selectedCountry[0].capital}</h3></li>
-          <li id="flagBox"><img src={selectedCountry[0].flag} alt="flag" /></li>
-          <li>
+          <li className="stats">
             <small>
               Subregion:
               {' '}
               <em>{selectedCountry[0].sub}</em>
             </small>
           </li>
-          <li>
+          <li className="stats">
             Official name:
             {' '}
             {selectedCountry[0].offName}
           </li>
-          <p>Spoken languages:</p>
-          {Object.entries(selectedCountry[0].lang).map(([k, l]) => (
-            <li key={k} className="langs">
-              <small>{l}</small>
-            </li>
-          ))}
-          <li>
+          <li className="stats">
+            <span>
+              Spoken languages:
+              {' '}
+              {Object.entries(selectedCountry[0].lang).map(([k, l]) => (
+                <small key={k}>
+                  {l}
+                  {' '}
+                </small>
+              ))}
+            </span>
+          </li>
+          <li className="stats">
             Area:
             {' '}
             {selectedCountry[0].area}
           </li>
           {(selectedCountry[0].unMember)
-            ? <li><small>UN Member</small></li> : <li><small>Not UN Member</small></li>}
-          <li>
+            ? <li className="stats"><small>UN Member</small></li> : <li className="stats"><small>Not UN Member</small></li>}
+          <li className="stats">
             Population:
             {' '}
             {selectedCountry[0].pop}
           </li>
-          <li>
+          <li className="stats">
             Time-zones:
             {' '}
             {selectedCountry[0].time}
