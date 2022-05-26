d4941695179b665b2205250b8e0aa47960432bb3
Mesi <mse212002@gmail.com>
2022-05-26 16:00:30 +0300

    :rotating_light: fixed linter errors

--- src/__tests__/Pages.test.js
+++ src/__tests__/Pages.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import store from '../Redux/configureStore';
 import { render } from '@testing-library/react';
+import store from '../Redux/configureStore';
 import Home from '../Pages/Home';
 import Details from '../Pages/Details';
 import { getCountries, getCountry } from '../Redux/countries/countries';
@@ -24,12 +24,12 @@ describe('Test for Home and Details', () => {
     expect(action.type).toBe('GET_COUNTRIES');
   });
 
-   it('should be able to see the details pages', () => {
+  it('should be able to see the details pages', () => {
     const details = render(
       <Provider store={store}>
         <Router>
           <Routes>
-            <Route path="/details/:name" element={<Details />}/>
+            <Route path="/details/:name" element={<Details />} />
           </Routes>
         </Router>
       </Provider>,
